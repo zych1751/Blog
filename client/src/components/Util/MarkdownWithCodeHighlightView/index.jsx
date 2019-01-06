5f2d841717c1e287583924105b4e962d66df2e29
SeoNamgyu <zych1751@naver.com>
2019-01-07 03:26:43 +0900

    코드 하이라이팅 버그 수정

--- client/src/components/Util/MarkdownWithCodeHighlightView/index.jsx
+++ client/src/components/Util/MarkdownWithCodeHighlightView/index.jsx
@@ -3,46 +3,46 @@ import ReactMarkdown from "react-markdown";
 import Loadable from "react-loadable";
 
 const Code = (props) => {
-    const loader = {
-        Lowlight: () => (import("react-lowlight"))
+  const loader = {
+    Lowlight: () => (import("react-lowlight"))
+  };
+
+  // 현재 html태그 직접 삽입이 막혀있어 이 방식으로 youtube 영상 넣는걸 대체합니다.
+  if(props.language === 'youtube') {
+    return (<iframe width="640" height="360"
+                    src={props.value}
+                    frameBorder="0" allowFullScreen />);
+  }
+
+  if(props.language) {
+    loader.lang = () => (import(`highlight.js/lib/languages/${props.language}`));
+  }
+
+  let Cmp = Loadable.Map({
+    loader: loader,
+    render: (loaded, _) => {
+      let { lang, Lowlight } = loaded;
+      if(lang) {
+        Lowlight.registerLanguage(props.language, lang.default);
+      }
+      return <Lowlight.default language={props.language || ""} value={props.value || ""} />;
+    },
+    loading: () => {
+      return (<pre><code>{props.value || ""}</code></pre>);
     }
+  });
 
-    // 현재 html태그 직접 삽입이 막혀있어 이 방식으로 youtube 영상 넣는걸 대체합니다.
-    if(props.language == 'youtube') {
-        return (<iframe width="640" height="360"
-            src={props.value}
-            frameBorder="0" allowFullScreen></iframe>);
-    }
-
-    if(props.language) {
-        loader.lang = () => (import(`highlight.js/lib/languages/${props.language}`));
-    }
-
-    let Cmp = Loadable.Map({
-        loader: loader,
-        render: (loaded, _) => {
-            let { lang, Lowlight } = loaded;
-            if(lang) {
-                Lowlight.registerLanguage(props.language, lang);
-            }
-            return <Lowlight language={props.language || ""} value={props.value || ""} />;
-        },
-        loading: () => {
-            return (<pre><code>{props.value || ""}</code></pre>);
-        }
-    });
-
-    return (<Cmp />);
+  return (<Cmp />);
 };
 
 const Img = (props) => {
-    return (<img className="post-view-image" src={props.src} />);
+  return (<img className="post-view-image" src={props.src} />);
 };
 
 const renderers = { code: Code, image: Img };
 
 const MarkDown = (props) => {
-    return (<ReactMarkdown {...props} renderers={renderers} />);
-}
+  return (<ReactMarkdown {...props} renderers={renderers} />);
+};
 
 export default MarkDown;
